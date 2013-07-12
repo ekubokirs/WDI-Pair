5170059f9799a77fc1b1ddc3a2d7f966c6196c8d
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-07-12 14:03:20 -0700

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WDIPair::Application.config.secret_key_base = '53b6bc4f509614ef772df549b98e6f2050053cac890fb6d7ad399f451a4206c1aa96952fb7e08501f27f5fc10d6f603cd941b3413c4cb0809651455c381ab68d'
---
+++ vendor/bundle/gems/json-1.8.0/ext/json/ext/parser/siteconf20130712-41177-1gn3jio.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130712-41177-xz53hp"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
